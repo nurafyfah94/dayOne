cabda80d71032c114dc612d581d4196d4dfa53c8
unknown <p90493@siswa.ukm.edu.my>
2019-10-16 19:13:01 +0800

    Second Push

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { Route } from '@angular/compiler/src/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private router: Router, private route: Route, private navigate: NavigationExtras) {}
+
+  submit(){
+
+this.router.navigate(['about']);
+  }
+
+}
